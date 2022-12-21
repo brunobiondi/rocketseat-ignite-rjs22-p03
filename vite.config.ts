772ca09ac547ce355a15ffabc1d539517f9d4e97
Bruno Biondi <eu@brunobiondi.com>
2022-12-21 11:05:30 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import alias from '@rollup/plugin-alias'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: resolve(__dirname, 'src'),
+        },
+      ],
+    }),
+  ],
+})
