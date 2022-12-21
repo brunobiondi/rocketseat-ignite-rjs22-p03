b7f07b03fe1246262fa1240cdf2c1c0a854b6a6b
Bruno Biondi <eu@brunobiondi.com>
2022-12-21 11:24:13 -0300

    chore: config styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export const App = () => {
-  return <h1>Hello word!</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello word!</h1>
+    </ThemeProvider>
+  )
 }
