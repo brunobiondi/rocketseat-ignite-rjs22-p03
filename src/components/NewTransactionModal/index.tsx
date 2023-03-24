6d1513337edcd9c3ab810fe39bea6c1cd242f362
Bruno Biondi <eu@brunobiondi.com>
2023-03-24 00:02:42 -0300

    feat: modal radiobutton

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,10 @@ import {
   CloseButton,
   Content,
   Overlay,
+  TransactionType,
+  TransactionTypeButton,
 } from '@/components/NewTransactionModal/styles'
-import { X } from 'phosphor-react'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export const NewTransactionModal = () => {
   return (
@@ -20,6 +22,18 @@ export const NewTransactionModal = () => {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
