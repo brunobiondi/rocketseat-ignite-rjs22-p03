15b746f6755a5100a5934f9547939b68b5dae4fe
Bruno Biondi <eu@brunobiondi.com>
2023-03-23 23:43:53 -0300

    feat: modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import { Container, Content, NewTransactionButton } from './styles'
 
 import logoImg from '@/assets/logo-dt-money.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '@/components/NewTransactionModal'
 
 export const Header = () => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </Content>
     </Container>
   )
